0ecdda494101bb304aa005c7f8dfdfdfc647391c
rlovett1 <rlovett1@vt.edu>
2017-07-15 21:31:53 -0400

    Add files via upload

---
+++ Free Agency.py
@@ -0,0 +1,10 @@
+
+import smtplib
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls() #make sure you leave this it protects the password
+server.login("officialfantasyemail@gmail.com", "Fantasy2017")
+ 
+msg = "Work!" #this would be the user input I was just making sure it worked
+server.sendmail("officialfantasyemail@gmail.com", "BOWMANSEMAIL@gmail.com", msg) #The bowmanemail is whatever email we send it to
+server.quit()
